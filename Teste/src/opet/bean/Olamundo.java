db0d4d73b7c6417efe0d0bc054c2a5791aa93971
aluno <aluno@172.31.1.115>
2017-03-02 10:31:36 -0300

    Add nova linha de código

---
+++ Teste/src/opet/bean/Olamundo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package opet.bean;
+
+/**
+ * @author aluno
+ *
+ */
+public class Olamundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println ("Alo Mundo");
+System.out.println ("Alteração");
+
+	}
+
+}
